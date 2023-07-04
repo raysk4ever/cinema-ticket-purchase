18383031206205e9bc2e100ec22781c1d6c3c514
Ravi <raysk7161@gmail.com>
2023-07-05 00:40:03 +0530

    🙌 added route for purchase consecutive seats cinema

--- src/routes/cinema.ts
+++ src/routes/cinema.ts
@@ -24,3 +24,24 @@ router.post('/cinemas', async (req: Request, res: Response) => {
   }
 });
 
+// Purchase the first two free consecutive seats in cinema C.
+// If there are no two consecutive seats available, return an error, otherwise return the list of seats.
+router.post('/cinemas/:cinemaId/purchase-consecutive', async (req: Request, res: Response) => {
+  try {
+    const { cinemaId } = req.params;
+
+    const seats = await Seat.find({ cinema: cinemaId, isPurchased: false })
+      .sort('seatNumber')
+      .limit(2);
+
+    if (seats.length < 2) {
+      res.status(404).json({ error: 'No consecutive seats available for purchase' });
+    } else {
+      await Seat.updateMany({ _id: { $in: seats.map(seat => seat._id) } }, { isPurchased: true });
+      res.json(seats);
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Failed to purchase consecutive seats' });
+  }
+});
