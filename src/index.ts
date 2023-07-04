f9e4845a9be89bca189b69832b232c52595dd526
Ravi <raysk7161@gmail.com>
2023-07-05 00:35:59 +0530

    🙌 setup express server and mongodb server

---
+++ src/index.ts
@@ -0,0 +1,30 @@
+import express from 'express';
+import { connectToDatabase } from './db';
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+
+// Routes
+app.route('*').all((_, res) => {
+  return res.send('Welcome to Cinema API')
+})
+
+// Error handling middleware
+app.use((err: any, req: express.Request, res: express.Response, next: express.NextFunction) => {
+  console.error(err.stack);
+  res.status(500).json({ error: 'Internal Server Error' });
+});
+
+// Start the server after connecting to the database
+connectToDatabase()
+  .catch((error) => {
+    console.error('Failed to connect to the database:', error);
+    process.exit(1);
+  });
+
+// Start the server
+app.listen(3000, () => {
+  console.log('Server started on port 3000');
+});
